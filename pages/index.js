431fd9f74086faefa2f3f5121afedbd8161a07d6
Phellipe Lariucci <phellipelariucci@gmail.com>
2025-10-04 14:37:31 -0300

    tentando iniciar a pag html

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>retorno</h1>
+    return (
+    <iframe
+      src="/index.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    />
+  );
 }
 
 export default Home;
