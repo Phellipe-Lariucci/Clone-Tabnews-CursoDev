2329ea256fe1bc47455c26dfa61b7a6608b44fa3
Phellipe Lariucci <phellipelariucci@gmail.com>
2025-10-05 08:17:26 -0300

    adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RPG-Meeting
+
 Um site para gerenciar lista de presença e outros aspectos de campanhas de RPG
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return (
+  return (
     <iframe
       src="index.html"
       style={{ width: "100%", height: "100vh", border: "none" }}
