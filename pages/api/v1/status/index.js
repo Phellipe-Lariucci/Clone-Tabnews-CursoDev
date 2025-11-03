aed1b93355b87bac3eea5aa56351ea54b56e6e37
Phellipe Lariucci <phellipelariucci@gmail.com>
2025-11-03 15:17:02 +0000

    melhorando os testes a serem adicionados no endpoint`/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
+const dbVersionResult = await database.query("SHOW server_version;");
+const dbVersion = dbVersionResult.rows[0].server_version;
+
+const dbMaxConnectionsResult = await database.query("SHOW max_connections");
+const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+const dbOpenedConnectionsResult = await database.query(
+  "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+);
+const dbOpenedConnections = dbOpenedConnectionsResult.rows[0].count;
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da media" });
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
